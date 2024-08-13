0cded7de1b9b76327711f36ce05d57dfb4bdcdfd
Ketyllen Emilly <96392358+KetyllenOliveira@users.noreply.github.com>
2024-08-12 21:53:07 -0300

    Add files via upload

---
+++ curriculoKetyllen/curriculoKetyllen/src/main/java/com/example/curriculoKetyllen/CurriculoKetyllenApplication.java
@@ -0,0 +1,13 @@
+package com.example.curriculoKetyllen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculoKetyllenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculoKetyllenApplication.class, args);
+	}
+
+}
---
+++ curriculoKetyllen/curriculoKetyllen/src/test/java/com/example/curriculoKetyllen/CurriculoKetyllenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.curriculoKetyllen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculoKetyllenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
